599d68a342a9f17567f188346444666069f9b826
RobsonFe <robson12ferreira@gmail.com>
2025-08-08 15:13:01 -0300

    feat: add Radix UI components and theme provider
    - Added dependencies for Radix UI components: Avatar, Dropdown Menu.
    - Created a ThemeProvider component to manage theme settings.
    - Implemented a Header component for user authentication and chat management.
    - Updated global CSS to include layout variables.
    - Enhanced layout with theme support and hydration warning suppression.
    - Introduced Avatar, Button, and Dropdown Menu components with styling and functionality.

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,21 @@
+import { useAuthStore } from "@/stores/authStore"
+import { useChatStore } from "@/stores/chatStore"
+import { useTheme } from "next-themes"
+import { usePathname } from "next/navigation"
+import { toast } from "sonner"
+
+export const Header = () => {
+  const { setTheme } = useTheme()
+  const { user, clearUser } = useAuthStore()
+  const { setChat, showChatsList, setShowChatsList } = useChatStore()
+  const pathname = usePathname()
+
+  const handleLogout = () => {
+    handleLogout()
+    clearUser()
+    setChat(null)
+    toast.success("Usuário Deslogado com sucesso", { position: "top-right" })
+  }
+
+  return <header className="h-header"></header>
+}
